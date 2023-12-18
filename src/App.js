26aacd4d343a185aff706e5845f0732fc0041666
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-18 10:47:43 +0330

    refactor(App.js): delete unnecessary codes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Button } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import rtlPlugin from "stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
 import "./App.css";
-import {Helmet , HelmetProvider} from "react-helmet-async"
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import Header from "./Componentes/UI/Header";
-import {theme} from "./Componentes/UI/Theme"
+import { theme } from "./Componentes/UI/Theme";
 const cacheRTL = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
@@ -17,10 +16,12 @@ function App() {
     <CacheProvider value={cacheRTL}>
       <ThemeProvider theme={theme}>
         <HelmetProvider>
-          <Helmet><title>My Portfolio</title></Helmet>
-        <div className="App">
-         <Header/>
-        </div>
+          <Helmet>
+            <title>My Portfolio</title>
+          </Helmet>
+          <div className="App">
+            <Header />
+          </div>
         </HelmetProvider>
       </ThemeProvider>
     </CacheProvider>
