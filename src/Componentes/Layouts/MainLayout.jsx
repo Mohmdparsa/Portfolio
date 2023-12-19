8eb08cc960eff81992b69bc6835a4da0bda0e5b5
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-19 14:43:43 +0330

    feat(MainLayout.jsx): Implement Grid layout for sidebar and main content

--- src/Componentes/Layouts/MainLayout.jsx
+++ src/Componentes/Layouts/MainLayout.jsx
@@ -4,7 +4,9 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import {theme} from "../UI/Theme"
+import { theme } from "../UI/Theme";
+import { Grid, Typography, Box, Divider, Avatar } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const Layout = ({ children }) => {
   const cacheRTL = createCache({
@@ -19,6 +21,24 @@ const Layout = ({ children }) => {
             <Helmet>
               <title>My Portfolio</title>
             </Helmet>
+            <Grid container sx={{ height: "100vh" }}>
+              <Grid
+                xs={2}
+                sx={{
+                  backgroundColor: "secondary.main",
+                  color: "white",
+                  textAlign: "center",
+                }}
+              >
+                سایدبار
+              </Grid>
+              <Grid
+                xs={10}
+                sx={{ backgroundColor: "primary.main", textAlign: "center" }}
+              >
+                محتوای اصلی
+              </Grid>
+            </Grid>
             {children}
           </HelmetProvider>
         </ThemeProvider>
