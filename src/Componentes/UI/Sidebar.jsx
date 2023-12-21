d1e159e19fe504c86d7e57367d75b5c53e18a62f
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-21 14:18:47 +0330

    feat(Sidebar.jsx): make sidebar content

--- src/Componentes/UI/Sidebar.jsx
+++ src/Componentes/UI/Sidebar.jsx
@@ -1,6 +1,24 @@
-import { Typography, Box, Divider, Avatar , Hidden} from "@mui/material";
+import {
+  Typography,
+  Box,
+  Divider,
+  Avatar,
+  Hidden,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { grey } from "@mui/material/colors";
+import {
+  HomeRounded,
+  AccountCircleRounded,
+  DescriptionRounded,
+  FolderRounded,
+  ThreePRounded,
+  CallRounded,
+  FavoriteRounded,
+  CopyrightRounded,
+} from "@mui/icons-material";
 const Sidebar = () => {
   return (
     <>
@@ -18,21 +36,89 @@ const Sidebar = () => {
       >
         <Box sx={{ textAlign: "center", justifyContent: "center" }}>
           <Hidden mdDown>
-          <Avatar
-          src={require("../../Assets/portfolio-image.jpg")}
-            sx={{
-              height: "160px",
-              width: "163px",
-              margin: "0 auto",
-              mt: "15px",
-            }}
-          >A A</Avatar>
+            <Avatar
+              src={require("../../Assets/portfolio-image.jpg")}
+              sx={{
+                height: "150px",
+                width: "153px",
+                margin: "0 auto",
+                mt: "15px",
+              }}
+            >
+              A A
+            </Avatar>
           </Hidden>
-          <Typography variant="h4" sx={{ mt: "15px" }}>
+          <Typography variant="h4" sx={{ mt: "15px"}}>
             امیرعلی آذرمنش
           </Typography>
-          <Typography variant="h6">برنامه نویس فرانت اند</Typography>
+          <Typography variant="h6">توسعه دهنده فرانت اند</Typography>
           <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
+          <Tabs
+            // variant="scrollable"
+            // scrollButtons="auto"
+            allowScrollButtonsMobile
+            orientation="vertical"
+          >
+            <Tab
+              label="صفحه اصلی"
+              icon={<HomeRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+            <Tab
+              label="درباره من"
+              icon={<AccountCircleRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+            <Tab
+              label="رزومه من"
+              icon={<DescriptionRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+            <Tab
+              label="نمونه کارها"
+              icon={<FolderRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+            <Tab
+              label="نظرات"
+              icon={<ThreePRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+            <Tab
+              label="ارتباط با من"
+              icon={<CallRounded />}
+              iconPosition="start"
+              sx={{ color: "white" }}
+            />
+          </Tabs>
+          <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "end",
+              alignItems: "center",
+              height: "80px",
+              mb: "12px",
+            }}
+          >
+            <Typography variant="subtitle1">
+              طراحی شده با
+              <FavoriteRounded
+                sx={{ alignItems: "middle", color: "tomato", ml: "7px"}}
+              />
+            </Typography>
+            <Typography variant="subtitle1">
+              کپی رایت 2023
+              <CopyrightRounded sx={{ alignItems: "middle", ml: "7px" }} />
+            </Typography>
+          </Box>
         </Box>
       </Grid>
     </>
