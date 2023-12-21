e8396f6695b397dda2658d4aafae2cf4660f714a
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-21 17:24:09 +0330

    feat(App.js): add Sidebar and ContentContainer and useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Layout from "./Componentes/Layouts/MainLayout";
+import Sidebar from "./Componentes/Sidebar"
+import ContentContainer from "./Componentes/ContentContainer"
+import { useState } from "react";
 
 function App() {
+  const [value , setValue] = useState(0)
+  const handleChange = (event , newValue)=>{
+    setValue(newValue)
+  }
   return (
     <div className="App">
-      <Layout></Layout>
+      <Layout>
+        <Sidebar value={value} handleChange={handleChange}/>
+        <ContentContainer/>
+      </Layout>
     </div>
   );
 }
