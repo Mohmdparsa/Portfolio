72d0dfb25d89abe5e73b61c74f9668dd1bd1d783
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-23 16:22:56 +0330

    feat(ContentContainer.jsx): add children prop and remove Typography

--- src/Componentes/ContentContainer.jsx
+++ src/Componentes/ContentContainer.jsx
@@ -1,21 +1,17 @@
 import Grid from "@mui/material/Unstable_Grid2";
-import { Typography } from "@mui/material";
-const ContentContainer = () => {
-    return(
-       <Grid
-    xs={12}
-    sm={12}
-    md={9}
-    lg={9}
-    xl={9}
-    sx={{ backgroundColor: "primary.main", textAlign: "center" }}
-  >
-    <Typography variant="h4">
-      محتوای اصلی  
-    </Typography>
-  </Grid>  
-    )
-
+const ContentContainer = ({ children }) => {
+  return (
+    <Grid
+      xs={12}
+      sm={12}
+      md={9}
+      lg={9}
+      xl={9}
+      sx={{ backgroundColor: "primary.main", textAlign: "center" }}
+    >
+      {children}
+    </Grid>
+  );
 };
 
 export default ContentContainer;
