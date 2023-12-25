f2b9a6567771fbdb525942a5f796702833e42a14
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-25 14:54:34 +0330

    chore(Theme.js) : move Theme.js to Layout/Theme folder and rename it to index.js

---
+++ src/Layouts/Theme/index.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+    direction: "rtl",
+    palette:{
+        primary : {
+            main:"#ffc107"
+        },
+        secondary:{
+            main:"#212121"
+        }
+    }
+  });
