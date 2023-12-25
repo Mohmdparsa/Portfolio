f91836d792b283876a98ab1d3f5956fe74fc15b5
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-25 12:07:42 +0330

    style(DrawerContent.jsx): change the Tab height and backgroundColor

--- src/Componentes/UI/DrawerContent.jsx
+++ src/Componentes/UI/DrawerContent.jsx
@@ -1,127 +1,171 @@
 import {
-    HomeRounded,
-    AccountCircleRounded,
-    DescriptionRounded,
-    FolderRounded,
-    ThreePRounded,
-    CallRounded,
-    FavoriteRounded,
-    CopyrightRounded,
-  } from "@mui/icons-material";
-  import {
-    Typography,
-    Box,
-    Divider,
-    Avatar,
-    Hidden,
-    Tabs,
-    Tab,
-  } from "@mui/material";
-  const DrawerContent = ({ value, handleChange }) => {
-    const a11yProps = (index) => {
-      return {
-        id: `vertical-tab-${index}`,
-        "aria-controls": `vertical-tabpanel-${index}`,
-      };
+  HomeRounded,
+  AccountCircleRounded,
+  DescriptionRounded,
+  FolderRounded,
+  ThreePRounded,
+  CallRounded,
+  FavoriteRounded,
+  CopyrightRounded,
+} from "@mui/icons-material";
+import {
+  Typography,
+  Box,
+  Divider,
+  Avatar,
+  Hidden,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { grey, blueGrey } from "@mui/material/colors";
+const DrawerContent = ({ value, handleChange }) => {
+  const a11yProps = (index) => {
+    return {
+      id: `vertical-tab-${index}`,
+      "aria-controls": `vertical-tabpanel-${index}`,
     };
-    return (
-      <>
-        <Box sx={{ textAlign: "center", justifyContent: "center" }}>
-          <Hidden mdDown>
-            <Avatar
-              src={require("../../Assets/portfolio-image.jpg")}
-              sx={{
-                height: "150px",
-                width: "153px",
-                margin: "0 auto",
-                mt: "15px",
-              }}
-            >
-              A A
-            </Avatar>
-          </Hidden>
-          <Typography variant="h4" sx={{ mt: "15px" }}>
-            امیرعلی آذرمنش
-          </Typography>
-          <Typography variant="h6">توسعه دهنده فرانت اند</Typography>
-          <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
-          <Tabs
-            variant="scrollable"
-            scrollButtons="auto"
-            allowScrollButtonsMobile
-            orientation="vertical"
-            value={value}
-            onChange={handleChange}
-          >
-            <Tab
-              label="صفحه اصلی"
-              icon={<HomeRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(0)}
-            />
-            <Tab
-              label="درباره من"
-              icon={<AccountCircleRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(1)}
-            />
-            <Tab
-              label="رزومه من"
-              icon={<DescriptionRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(2)}
-            />
-            <Tab
-              label="نمونه کارها"
-              icon={<FolderRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(3)}
-            />
-            <Tab
-              label="نظرات"
-              icon={<ThreePRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(4)}
-            />
-            <Tab
-              label="ارتباط با من"
-              icon={<CallRounded />}
-              iconPosition="start"
-              sx={{ color: "white" }}
-              {...a11yProps(5)}
-            />
-          </Tabs>
-          <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
-          <Box
+  };
+  return (
+    <>
+      <Box sx={{ textAlign: "center", justifyContent: "center" }}>
+        <Hidden mdDown>
+          <Avatar
+            src={require("../../Assets/portfolio-image.jpg")}
             sx={{
-              flexGrow: 1,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "end",
-              alignItems: "center",
-              height: "80px",
-              mb: "12px",
+              height: "150px",
+              width: "153px",
+              margin: "0 auto",
+              mt: "15px",
             }}
           >
-            <Typography variant="subtitle1">
-              طراحی شده با
-              <FavoriteRounded
-                sx={{ alignItems: "middle", color: "tomato", ml: "7px" }}
-              />
-            </Typography>
-            <Typography variant="subtitle1">
-              کپی رایت 2023
-              <CopyrightRounded sx={{ alignItems: "middle", ml: "7px" }} />
-            </Typography>
-          </Box>
+            A A
+          </Avatar>
+        </Hidden>
+        <Typography variant="h4" sx={{ mt: "15px" }}>
+          امیرعلی آذرمنش
+        </Typography>
+        <Typography variant="h6">توسعه دهنده فرانت اند</Typography>
+        <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
+        <Tabs
+          variant="scrollable"
+          scrollButtons="auto"
+          allowScrollButtonsMobile
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+        >
+          <Tab
+            label="صفحه اصلی"
+            icon={<HomeRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+              mt: 1,
+            }}
+            {...a11yProps(0)}
+          />
+          <Tab
+            label="درباره من"
+            icon={<AccountCircleRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+            }}
+            {...a11yProps(1)}
+          />
+          <Tab
+            label="رزومه من"
+            icon={<DescriptionRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+            }}
+            {...a11yProps(2)}
+          />
+          <Tab
+            label="نمونه کارها"
+            icon={<FolderRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+            }}
+            {...a11yProps(3)}
+          />
+          <Tab
+            label="نظرات"
+            icon={<ThreePRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+            }}
+            {...a11yProps(4)}
+          />
+          <Tab
+            label="ارتباط با من"
+            icon={<CallRounded />}
+            iconPosition="start"
+            sx={{
+              color: "white",
+              "&.MuiTab-root": { minHeight: 48 },
+              backgroundColor: grey[800],
+              my: 0.3,
+              mx: 2,
+              borderRadius: 2,
+            }}
+            {...a11yProps(5)}
+          />
+        </Tabs>
+        <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "end",
+            alignItems: "center",
+            height: "80px",
+            mb: "12px",
+          }}
+        >
+          <Typography variant="subtitle1">
+            طراحی شده با
+            <FavoriteRounded
+              sx={{ alignItems: "middle", color: "tomato", ml: "7px" }}
+            />
+          </Typography>
+          <Typography variant="subtitle1">
+            کپی رایت 2023
+            <CopyrightRounded sx={{ alignItems: "middle", ml: "7px" }} />
+          </Typography>
         </Box>
-      </>
-    );
-  };
-  
-  export default DrawerContent;
+      </Box>
+    </>
+  );
+};
+
+export default DrawerContent;
