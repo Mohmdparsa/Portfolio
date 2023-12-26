301cf6a0297388a299889aec1c9bb476d7216fbd
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-26 20:04:17 +0330

    chore(DrawerContent.jsx): remove codes of footer part of the Sidebar

--- src/Componentes/DrawerContent.jsx
+++ src/Componentes/DrawerContent.jsx
@@ -125,29 +125,7 @@ const DrawerContent = ({ value, handleChange }) => {
             {...a11yProps(5)}
           />
         </Tabs>
-        <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "end",
-            alignItems: "center",
-            height: "80px",
-            mb: "12px",
-          }}
-        >
-          <Typography variant="subtitle1">
-            طراحی شده با
-            <FavoriteRounded
-              sx={{ alignItems: "middle", color: "tomato", ml: "7px" }}
-            />
-          </Typography>
-          <Typography variant="subtitle1">
-            کپی رایت 2023
-            <CopyrightRounded sx={{ alignItems: "middle", ml: "7px" }} />
-          </Typography>
-        </Box>
+       
       </Box>
     </>
   );
