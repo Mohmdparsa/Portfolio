3867677c61e79fd965e7c573f251bd45bd6c9536
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-26 19:57:06 +0330

    chore(DrawerContenet.jsx): remove codes of header part of the Sidebar

--- src/Componentes/DrawerContent.jsx
+++ src/Componentes/DrawerContent.jsx
@@ -28,24 +28,6 @@ const DrawerContent = ({ value, handleChange }) => {
   return (
     <>
       <Box sx={{ textAlign: "center", justifyContent: "center" }}>
-        <Hidden mdDown>
-          <Avatar
-            src={require("../Assets/portfolio-image.jpg")}
-            sx={{
-              height: "150px",
-              width: "153px",
-              margin: "0 auto",
-              mt: "15px",
-            }}
-          >
-            A A
-          </Avatar>
-        </Hidden>
-        <Typography variant="h4" sx={{ mt: "15px" }}>
-          امیرعلی آذرمنش
-        </Typography>
-        <Typography variant="h6">توسعه دهنده فرانت اند</Typography>
-        <Divider variant="middle" color="grey" sx={{ mt: "10px" }} />
         <Tabs
           variant="scrollable"
           scrollButtons="auto"
