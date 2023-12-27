e0ebf1ee0a3b74d6d07db948a1797b8e64470398
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-27 12:13:21 +0330

    feat(SidebarTabs.jsx): add Sidebar Tabs codes from DrawerContent here

--- src/Componentes/Sidebar/SidebarTabs.jsx
+++ src/Componentes/Sidebar/SidebarTabs.jsx
@@ -1,6 +1,100 @@
 const SidebarTabs = () => {
   return (
     <>
+     <Tabs
+        variant="scrollable"
+        scrollButtons="auto"
+        allowScrollButtonsMobile
+        orientation="vertical"
+        value={value}
+        onChange={handleChange}
+      >
+        <Tab
+          label="صفحه اصلی"
+          icon={<HomeRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+            mt: 1,
+          }}
+          {...a11yProps(0)}
+        />
+        <Tab
+          label="درباره من"
+          icon={<AccountCircleRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+          }}
+          {...a11yProps(1)}
+        />
+        <Tab
+          label="رزومه من"
+          icon={<DescriptionRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+          }}
+          {...a11yProps(2)}
+        />
+        <Tab
+          label="نمونه کارها"
+          icon={<FolderRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+          }}
+          {...a11yProps(3)}
+        />
+        <Tab
+          label="نظرات"
+          icon={<ThreePRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+          }}
+          {...a11yProps(4)}
+        />
+        <Tab
+          label="ارتباط با من"
+          icon={<CallRounded />}
+          iconPosition="start"
+          sx={{
+            color: "white",
+            "&.MuiTab-root": { minHeight: 48 },
+            backgroundColor: grey[800],
+            my: 0.3,
+            mx: 2,
+            borderRadius: 2,
+          }}
+          {...a11yProps(5)}
+        />
+      </Tabs>
     </>
   );
 };
