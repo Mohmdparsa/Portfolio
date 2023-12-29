d5447a2ad71624b5bc5d14c5a8fa9bd28b438f47
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-29 16:19:41 +0330

    feat(DrawerActionButton.jsx): paste Fab and Box tag that we removed from Sidebar.jsx

--- src/Componentes/Drawer/DrawerActionButton.jsx
+++ src/Componentes/Drawer/DrawerActionButton.jsx
@@ -1,6 +1,27 @@
 const DrawerActionButton = ()=>{
     return(
         <>
+            
+            <Box
+          sx={{
+            display: {
+              xs: "block",
+              sm: "block",
+              md: "none",
+              lg: "none",
+              xl: "none",
+            },
+          }}
+        >
+          <Fab
+            aria-label="Sidebar"
+            size="small"
+            sx={{ m: 2, color: red[500] }}
+            onClick={handleDrawerToggle}
+          >
+            <MenuRounded />
+          </Fab>
+        </Box>
         </>
     )
 }
