21c997e3619771eb69e5d9d8b1af7e9023a6c00f
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-29 14:12:35 +0330

    feat(SidebarDrawer.jsx): paste Drawer tag here from Sidebar.jsx

--- src/Componentes/Drawer/SidebarDrawer.jsx
+++ src/Componentes/Drawer/SidebarDrawer.jsx
@@ -1,7 +1,20 @@
 const SidebarDrawer = ()=>{
     return(
         <>
-        
+                <Drawer
+          open={open}
+          variant="temporary"
+          onClose={() => setOpen(false)}
+          sx={{
+            "& .MuiDrawer-paper": {
+              width: 300,
+              backgroundColor: "secondary.main",
+              color: "white",
+            },
+          }}
+        >
+          <DrawerContent value={value} handleChange={handleChange} />
+        </Drawer>
         </>
     )
 }
