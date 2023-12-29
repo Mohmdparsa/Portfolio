a9f846edfd1228db06ffe04678a59b257a929f87
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-12-29 23:43:14 +0330

    refactor(Sidebar.jsx): remove useState Hook

--- src/Componentes/Sidebar/Sidebar.jsx
+++ src/Componentes/Sidebar/Sidebar.jsx
@@ -1,10 +1,9 @@
 import SidebarDrawer from "../Drawer/SidebarDrawer";
 import SidebarContent from "./SidebarContent";
-import { useState } from "react";
 import DrawerActionButton from "../Drawer/DrawerActionButton";
 
 const Sidebar = ({ value, handleChange }) => {
-  const [open, setOpen] = useState(false);
+
 
   return (
     <>
