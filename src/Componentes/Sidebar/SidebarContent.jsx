eae116c305ee2a3b14ce7df90d0b138e6fbb4f72
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-02 13:38:19 +0330

    chore(SidebarContent.jsx): delete unused prop for code cleanup

--- src/Componentes/Sidebar/SidebarContent.jsx
+++ src/Componentes/Sidebar/SidebarContent.jsx
@@ -1,12 +1,11 @@
 import { Box } from "@mui/material";
-import {SidebarTabs , SidebarHeader , SidebarFooter} from "./Index"
-const SidebarContent = ({ value, handleChange }) => {
-
+import { SidebarTabs, SidebarHeader, SidebarFooter } from "./Index";
+const SidebarContent = () => {
   return (
     <>
       <Box sx={{ textAlign: "center", justifyContent: "center" }}>
         <SidebarHeader />
-        <SidebarTabs value={value} handleChange={handleChange}/>
+        <SidebarTabs />
 
         <SidebarFooter />
       </Box>
