3f44811c5a76567652780e16d2d26ff0a50a3933
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-02 18:50:48 +0330

    refactor(App.js): rename TabPanel to Page

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,8 +1,8 @@
 import Layout from "../Layouts/MainLayout";
 import Sidebar from "../Componentes/Sidebar/Sidebar";
-import ContentContainer from "./ContentContainer";
+import PageContainer from "./PagesContainer";
 import { useState } from "react";
-import TabPanel from "../Componentes/Tabs/TabPanel";
+import Page from "../Componentes/Tabs/Page";
 import { Typography } from "@mui/material";
 import SidebarContainers from "./SidebarContainers";
 import { MainContext } from "../Context/Index";
@@ -24,38 +24,38 @@ function App() {
             <Sidebar pageNumber={pageNumber} handlePageNumber={handlePageNumber} />
           </SidebarContainers>
 
-          <ContentContainer>
-            <TabPanel pageNumber={pageNumber} index={0}>
+          <PageContainer>
+            <Page pageNumber={pageNumber} index={0}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 صفحه اصلی
               </Typography>
-            </TabPanel>
-            <TabPanel pageNumber={pageNumber} index={1}>
+            </Page>
+            <Page pageNumber={pageNumber} index={1}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 درباره من
               </Typography>
-            </TabPanel>
-            <TabPanel pageNumber={pageNumber} index={2}>
+            </Page>
+            <Page pageNumber={pageNumber} index={2}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 رزومه من
               </Typography>
-            </TabPanel>
-            <TabPanel pageNumber={pageNumber} index={3}>
+            </Page>
+            <Page pageNumber={pageNumber} index={3}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 نمونه کارها
               </Typography>
-            </TabPanel>
-            <TabPanel pageNumber={pageNumber} index={4}>
+            </Page>
+            <Page pageNumber={pageNumber} index={4}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 نظرات
               </Typography>
-            </TabPanel>
-            <TabPanel pageNumber={pageNumber} index={5}>
+            </Page>
+            <Page pageNumber={pageNumber} index={5}>
               <Typography variant="h3" sx={{ textAlign: "center" }}>
                 ارتباط با من
               </Typography>
-            </TabPanel>
-          </ContentContainer>
+            </Page>
+          </PageContainer>
         </Layout>
       </div>
     </MainContext.Provider>
