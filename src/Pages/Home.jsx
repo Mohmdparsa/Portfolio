05b7c4df825f7bff371147cb5432677f7299d162
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-16 11:54:36 +0330

    feat(Home.jsx): make animated typing for main page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,62 @@
-import PageImage1 from "../Assets/PageImage1.jpg"
-import { Typography , Box } from "@mui/material"
-const Home = ()=>{
-    return(
-        <>
-            <Box
-                sx={{
-                  backgroundImage: `url(${PageImage1})`,
-                  height: "100vh",
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                }}
-              >
-                <Typography
-                  variant="h3"
-                  sx={{
-                    textAlign: "center",
-                    color: "primary.main",
-                    paddingTop: "20px",
-                  }}
-                >
-                  صفحه اصلی
-                </Typography>
-              </Box>
-        </>
-    )
+import PageImage1 from "../Assets/PageImage1.jpg";
+import { Typography, Box } from "@mui/material";
+import Typed from "typed.js";
+import { useRef, useEffect } from "react";
+const Home = () => {
+  const nameEl = useRef(null);
+  const infoEl = useRef(null);
+  const strings = [
+    "من یک توسعه دهنده وب هستم",
+    "من یک فریلنسر هستم",
+    "من یک محتواساز دنیای برنامه نویسی هستم",
+  ];
+  useEffect(() => {
+    const typedName = new Typed(nameEl.current, {
+      strings: ["امیرعلی آذرمنش"],
+      typeSpeed: 60,
+      backSpeed: 50,
+      backDelay: 10,
+      showCursor: false,
+    });
+    const typedInfo = new Typed(infoEl.current, {
+      strings,
+      typeSpeed: 70,
+      backSpeed: 50,
+      backDelay: 10,
+      loop:true,
+      showCursor: false,
+    });
+    return ()=>{
+      typedInfo.destroy();
+      typedName.destroy();
+    }
+  }, []);
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundImage: `url(${PageImage1})`,
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Typography ref={nameEl} variant="h3" color="primary.main" sx={{pt:"170px"}}></Typography>
+        <Typography
+          variant="h4"
+          ref={infoEl}
+          sx={{
+            textAlign: "center",
+            color: "white",
+            paddingTop: "20px",
+          }}
+        >
+          صفحه اصلی
+        </Typography>
+      </Box>
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
