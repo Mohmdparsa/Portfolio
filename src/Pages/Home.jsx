92ba0151c4a97aab17fcb9562f8c689929af4d0e
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-17 00:05:40 +0330

    feat(Home.jsx): Implement react-particles in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,11 @@ import PageImage1 from "../Assets/PageImage1.jpg";
 import { Typography, Box } from "@mui/material";
 import Typed from "typed.js";
 import { useRef, useEffect } from "react";
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { Links } from "../Constans/Particles";
+
 const Home = () => {
   const nameEl = useRef(null);
   const infoEl = useRef(null);
@@ -23,13 +28,20 @@ const Home = () => {
       typeSpeed: 70,
       backSpeed: 50,
       backDelay: 10,
-      loop:true,
+      loop: true,
       showCursor: false,
     });
-    return ()=>{
+    return () => {
       typedInfo.destroy();
       typedName.destroy();
-    }
+    };
+  }, []);
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
   }, []);
   return (
     <>
@@ -42,7 +54,18 @@ const Home = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <Typography ref={nameEl} variant="h3" color="primary.main" sx={{pt:"170px"}}></Typography>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={Links}
+        />
+        <Typography
+          ref={nameEl}
+          variant="h3"
+          color="primary.main"
+          sx={{ pt: "170px" }}
+        ></Typography>
         <Typography
           variant="h4"
           ref={infoEl}
