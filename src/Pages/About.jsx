5cb7efa32db66ba3a5e00d5f074ca58035584b5c
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-17 17:40:41 +0330

    feat(About.jsx): create About.jsx component for About part

---
+++ src/Pages/About.jsx
@@ -0,0 +1,72 @@
+import {
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Avatar,
+  Chip,
+} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { grey } from "@mui/material/colors";
+import Avatar02 from "../Assets/Avatar02.jpg";
+import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
+import CodeRounded from "@mui/icons-material/CodeRounded";
+
+const About = () => {
+  return (
+    <>
+      <Card
+        sx={{
+          height: "100vh",
+          backgroundColor: "whitesmoke",
+        }}
+      >
+        <CardContent>
+          <Grid container sx={{ mx: 3 }}>
+            <Grid  xs={12} sm={12} md={8} lg={8} xl={8}>
+                <Divider textAlign="right">
+                <Chip
+                icon={<CodeRoundedIcon/>}
+                  label={ <Typography sx={{textAlign:"center" , variant:"body1" , color:"black"}}
+                  >توسعه دهنده فرانت اند</Typography>
+                }
+                  size="small"
+                  sx={{p:3 }}
+                />
+              </Divider>
+              <Typography variant="body1" color="secondary.main" textAlign="left" sx={{mt:2}}>
+
+              </Typography>
+            </Grid>
+            
+            <Grid xs={0} sm={0} md={4} lg={4} xl={4}>
+              <Avatar
+      
+                textAlign="left"
+                src={Avatar02}
+                variant="square"
+                sx={{
+                  height: "240px",
+                  width: "200px",
+                  margin: "0 auto",
+                  mt: "15px",
+                  borderRadius:"15px",
+                  display: {
+                    xl: "block",
+                    lg: "block",
+                    md: "block",
+                    sm: "none",
+                    xs: "none",
+                  },
+                }}
+              ></Avatar>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default About;
