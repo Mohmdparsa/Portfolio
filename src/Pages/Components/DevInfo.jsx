26ac213c6621c39867e5715b6839b977c9344d17
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-17 18:24:20 +0330

    feat(DevInfo.jsx): create current component for About part typography

---
+++ src/Pages/Components/DevInfo.jsx
@@ -0,0 +1,17 @@
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+const DevInfo = ()=>{
+    return(
+        <>
+          <Typography variant="body1" color="secondary.main" textAlign="left" sx={{mt:2}}
+              fontSize="26px"
+               >
+                نام و نام خانوادگی : امیرعلی آذرمنش
+                <ArrowBackIosNewRoundedIcon sx={{verticalAlign:"bottom" , color:"primary.main"}}/>
+
+              </Typography>
+        
+        </>
+    )
+}
+
+export default DevInfo
