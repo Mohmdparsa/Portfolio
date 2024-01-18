f8ffd6e4e763e989dfcc96a5893b6c856d2aee91
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-18 17:41:24 +0330

    feat(Skill.jsx): add some props from About.jsx

--- src/Pages/Components/Skill.jsx
+++ src/Pages/Components/Skill.jsx
@@ -1,26 +1,31 @@
-import { Divider, Chip , Box , Typography , LinearProgress } from "@mui/material"
-import Html from "../../Assets/Icons/icons8-html.svg"
-const Skill = ()=>{
-    return(
-        <>
-        <Divider textAlign="right" sx={{marginTop:"20px"}}>
-            <Chip label="HTML" sx={{width:"110px"}} icon={<Box component="img" src={Html}  sx={{height:30}}/>}>
-            </Chip>
-        </Divider>
-        <Box sx={{display:"flex", alignItems:"center"}}>
-            <Box sx={{minWidth:"35"}}>
-                <Typography variant="body2" sx={{mt:2}}>
-                    95%
-                </Typography>
-            </Box>
-         
-            <Box sx={{width:"90%" , mr:4 , mt:3}}>
-   <LinearProgress variant="determinate" value={95} sx={{height:10}}/>
-            </Box>
+import { Divider, Chip, Box, Typography, LinearProgress } from "@mui/material";
+const Skill = ({ icon, value, name }) => {
+  return (
+    <>
+      <Divider textAlign="right" sx={{ marginTop: "20px" }}>
+        <Chip
+          label={name}
+          sx={{ width: "140px" }}
+          icon={<Box component="img" src={icon} sx={{ height: 30 }} />}
+        ></Chip>
+      </Divider>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={{ minWidth: "35" }}>
+          <Typography variant="body2" sx={{ mt: 2 }}>
+            {value}
+          </Typography>
         </Box>
-        
-        </>
-    )
-}
 
-export default Skill 
+        <Box sx={{ width: "90%", mr: 4, mt: 3 }}>
+          <LinearProgress
+            variant="determinate"
+            value={value}
+            sx={{ height: 10 ,borderRadius:"10px" }}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Skill;
