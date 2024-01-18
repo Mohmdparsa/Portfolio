b26aa4efaba3a8854522ac9ae79dae26eb10dc74
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-18 17:36:56 +0330

    feat(Skill.js): create Skill.js for organize icons

---
+++ src/Constans/Skill.js
@@ -0,0 +1,24 @@
+import HtmlIcon from "../Assets/Icons/icons8-html.svg";
+import CssIcons from "../Assets/Icons/icons8-css.svg";
+import JsIcons from "../Assets/Icons/icons8-js.svg";
+import ReactJsIcons from "../Assets/Icons/icons8-react-native.svg";
+ export const devSkill = {
+    htmlSkill : {
+        name:"HTMl",
+        icon: HtmlIcon,
+    },
+    cssSkill : {
+        name:"Css",
+        icon:CssIcons,
+    },
+    
+    jsSkill : {
+        name:"JavaScript",
+        icon:JsIcons
+        },
+        reactSkill : {
+            name:"React",
+            icon:ReactJsIcons
+        }
+    
+ }
