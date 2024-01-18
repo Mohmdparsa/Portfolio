3c9334bb62814dc6933f318fca998c8641c612e9
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-18 15:14:03 +0330

    chore(Skill.jsx): use Box and linear progress and make some change in style

--- src/Pages/Components/Skill.jsx
+++ src/Pages/Components/Skill.jsx
@@ -3,13 +3,24 @@ import Html from "../../Assets/Icons/icons8-html.svg"
 const Skill = ()=>{
     return(
         <>
-        <Divider textAlign="right">
-            <Chip icon={<Box component={img} src={Html} label={Html} />}>
+        <Divider textAlign="right" sx={{marginTop:"20px"}}>
+            <Chip label="HTML" sx={{width:"110px"}} icon={<Box component="img" src={Html}  sx={{height:30}}/>}>
             </Chip>
         </Divider>
+        <Box sx={{display:"flex", alignItems:"center"}}>
+            <Box sx={{minWidth:"35"}}>
+                <Typography variant="body2" sx={{mt:2}}>
+                    95%
+                </Typography>
+            </Box>
+         
+            <Box sx={{width:"90%" , mr:4 , mt:3}}>
+   <LinearProgress variant="determinate" value={95} sx={{height:10}}/>
+            </Box>
+        </Box>
         
         </>
     )
 }
 
-export default Skill
+export default Skill 
