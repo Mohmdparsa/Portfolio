2881aac6d3565c603cc3fb25114511e63804b19c
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-18 10:32:27 +0330

    chore(DevInfo.jsx): add children  prop

--- src/Pages/Components/DevInfo.jsx
+++ src/Pages/Components/DevInfo.jsx
@@ -1,12 +1,12 @@
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 import { Typography } from '@mui/material';
-const DevInfo = ()=>{
+const DevInfo = ({children})=>{
     return(
         <>
           <Typography variant="body1" color="secondary.main" textAlign="left" sx={{mt:2}}
               fontSize="26px"
                >
-                نام و نام خانوادگی : امیرعلی آذرمنش
+            {children}
                 <ArrowBackIosNewRoundedIcon sx={{verticalAlign:"bottom" , color:"primary.main"}}/>
 
               </Typography>
