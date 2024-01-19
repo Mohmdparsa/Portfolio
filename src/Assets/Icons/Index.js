f30b703be8bf99c9fab2eb7c1bd755c66b5aca6f
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-19 10:48:42 +0330

    chore(Index.js): add icons here for component organization

---
+++ src/Assets/Icons/Index.js
@@ -0,0 +1,5 @@
+export {default as HtmlIcon} from "./icons8-html.svg"
+export {default as CssIcons} from "./icons8-css.svg"
+export {default as JsIcons} from "./icons8-js.svg"
+export {default as ReactJsIcons} from "./icons8-react-native.svg"
+
