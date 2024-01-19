69f900eadda6f16fbb965e06f195f653ecadbccb
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-19 22:40:15 +0330

    refactor(MainLayout.jsx): remove helmet

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import rtlPlugin from "stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 import { theme } from "./Theme";
 import Grid from "@mui/material/Unstable_Grid2";
 
@@ -17,9 +17,6 @@ const Layout = ({ children }) => {
       <CacheProvider value={cacheRTL}>
         <ThemeProvider theme={theme}>
           <HelmetProvider>
-            <Helmet>
-              <title>My Portfolio</title>
-            </Helmet>
             <Grid container sx={{ height: "100vh" }}>
               {children}
             </Grid>
