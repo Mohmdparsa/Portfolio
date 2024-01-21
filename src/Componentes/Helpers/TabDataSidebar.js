4ed70017f6419ff6ed415b20bf8030414c6f432f
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-21 14:16:22 +0330

    chore(TabDataSidebar.js): remove resume tabs

--- src/Componentes/Helpers/TabDataSidebar.js
+++ src/Componentes/Helpers/TabDataSidebar.js
@@ -1,7 +1,6 @@
 import {
     HomeRounded,
     AccountCircleRounded,
-    DescriptionRounded,
     FolderRounded,
     ThreePRounded,
     CallRounded,
@@ -18,10 +17,9 @@ export const TabsData = ()=>{
     const tabs = [
     { label: "صفحه اصلی", icon: <HomeRounded />, ...a11yProps(0) },
     { label: "درباره من", icon: <AccountCircleRounded />, ...a11yProps(1) },
-    { label: "رزومه من", icon: <DescriptionRounded />, ...a11yProps(2) },
-    { label: "نمونه کارها", icon: <FolderRounded />, ...a11yProps(3) },
-    { label: "نظرات", icon: <ThreePRounded />, ...a11yProps(4) },
-    { label: "ارتباط با من", icon: <CallRounded />, ...a11yProps(5) },
+    { label: "نمونه کارها", icon: <FolderRounded />, ...a11yProps(2) },
+    { label: "نظرات", icon: <ThreePRounded />, ...a11yProps(3) },
+    { label: "ارتباط با من", icon: <CallRounded />, ...a11yProps(4) },
   ];
   return tabs
 }
