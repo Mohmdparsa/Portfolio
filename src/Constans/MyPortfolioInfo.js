48e0eeb37681024c8d37c9b24ff89c983f2e9395
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-22 13:04:07 +0330

    feat(MyPortfolio.js): implement MyPortfolio.js and storage information of MyPortfolio

---
+++ src/Constans/MyPortfolioInfo.js
@@ -0,0 +1,44 @@
+import breadOnlineShop from "../Assets/MyPortfolioImage/bread.jpg"
+import onlineShop1 from "../Assets/MyPortfolioImage/onlineShop1.jpg"
+import photography from "../Assets/MyPortfolioImage/photography.jpg"
+import restaurant1 from "../Assets/MyPortfolioImage/restaurant1.jpg"
+
+export const portfolioInfo = [
+    {
+        title:"وب سایت نان فانتزی",
+        image:breadOnlineShop,
+        link:"#",
+        info:"این وب سایت برای یک فروشگاه نان فانتزی درست شده"
+    },
+    // {
+    //     title:"وب سایت فروشگاهی",
+    //     image:"",
+    //     link:"#",
+    //     info:"این وب سایت  یک فروشگاه مخصوص لوازم بانوان می باشد"
+    // },
+    {
+        title:"وب سایت فروشگاهی",
+        image:onlineShop1,
+        link:"#",
+        info:"این وب سایت یک انلاین شاپ مخصوص لوازم خانگی میباشد"
+    },
+    {
+        title:"وب سایت خدمات عکاسی",
+        image:photography,
+        link:"#",
+        info:"این وب سایت یک وب سایت خدمات و لوازم عکاسی میباشد"
+    },
+    // {
+    //     title:"وب سایت رستوران میباشد",
+    //     image:"",
+    //     link:"#",
+    //     info:"این وب سایت یک رستوران میباشد"
+    // },
+    {
+        title:"وب سایت رستوران میباشد",
+        image:restaurant1,
+        link:"#",
+        info:"این وب سایت یک رستوران میباشد"
+    },
+
+]
