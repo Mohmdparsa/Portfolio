e70ca7a91c0ac6dacbf2a57bf5967b9f7cd6b81d
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-23 12:47:54 +0330

    feat(Detail.js): implement Detail.js component for storage Comment page data here

---
+++ src/Constans/Details.js
@@ -0,0 +1,31 @@
+ import userImage from "../Assets/CommentPageImage/userImage.png"
+ export const userComments = [
+    {
+        fullname:"امیرعلی آذرمنش",
+        jobTitle:"توسعه دهنده فرانت اند",
+        avatar:`${userImage}`,
+        comment:"خیلی کارتون عالیه موفق باشید"
+
+    },
+    {
+        fullname:"امیرعلی آذرمنش",
+        jobTitle:"توسعه دهنده فرانت اند",
+        avatar:`${userImage}`,
+        comment:"سلام کارتون عالیه 👦"
+
+    },
+    {
+        fullname:"امیرعلی آذرمنش",
+        jobTitle:"توسعه دهنده فرانت اند",
+        avatar:`${userImage}`,
+        comment:"سلام پکیج آموزش برنامه نویسی ندارید؟"
+
+    },
+    {
+        fullname:"امیرعلی آذرمنش",
+        jobTitle:"توسعه دهنده فرانت اند",
+        avatar:`${userImage}`,
+        comment:"از کجا میتونم برنامه نویسی یاد بگیرم"
+
+    },
+]
