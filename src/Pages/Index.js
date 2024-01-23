e809d42ee72dd43fe176cb4389e236468e8f5448
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2024-01-23 12:43:55 +0330

    chore(Index.js): add Comment.jsx here for compnent organization

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,3 +1,4 @@
 export {default  as Home} from "./Home"
 export {default as About} from "./About"
 export {default as MyPortfolio} from "./MyPortfolio"
+export {default as Comments} from "./Comment"
