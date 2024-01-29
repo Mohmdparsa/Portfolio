bf8e16a8fac87899433b4290c68a172611a1d12c
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-01-29 04:26:01 -0800

    refactor(App.js):remove unused Typography

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,18 +1,14 @@
 import Layout from "../Layouts/MainLayout";
 import Sidebar from "../Componentes/Sidebar/Sidebar";
 import PageContainer from "./PagesContainer";
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import Page from "../Pages/Components/Page";
-import { Typography } from "@mui/material";
 import SidebarContainers from "./SidebarContainers";
 import { MainContext } from "../Context/Index";
 import DrawerActionButton from "../Componentes/Drawer/DrawerActionButton";
-import {Home , About , MyPortfolio , Comments , Contact} from "../Pages/Index"
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-
-
-
+import { Home, About, MyPortfolio, Comments, Contact } from "../Pages/Index";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
   const [pageNumber, setPageNumber] = useState(0);
@@ -22,14 +18,13 @@ function App() {
     setPageNumber(newValue);
   };
 
- 
-    const theme = useTheme();
-    const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
-    useEffect(()=>{
-      if(isMdUp){
-         setDrawerOpen(false)
-      }
-    },[isMdUp])
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
+  useEffect(() => {
+    if (isMdUp) {
+      setDrawerOpen(false);
+    }
+  }, [isMdUp]);
 
   return (
     <MainContext.Provider
@@ -46,24 +41,21 @@ function App() {
           <DrawerActionButton />
 
           <PageContainer>
-                   <Page pageNumber={pageNumber} index={0}>
-                    <Home helmetTitle="وب سایت شخصی امیرعلی آذرمنش"/>
-          
+            <Page pageNumber={pageNumber} index={0}>
+              <Home helmetTitle="وب سایت شخصی امیرعلی آذرمنش" />
             </Page>
             <Page pageNumber={pageNumber} index={1}>
-            <About helmetTitle="وب سایت شخصی | درباره من"/>
+              <About helmetTitle="وب سایت شخصی | درباره من" />
             </Page>
             <Page pageNumber={pageNumber} index={2}>
-          <MyPortfolio helmetTitle="وب سایت شخصی | نمونه کارها"/>
+              <MyPortfolio helmetTitle="وب سایت شخصی | نمونه کارها" />
             </Page>
             <Page pageNumber={pageNumber} index={3}>
-              <Comments helmetTitle="وب سایت شخصی | نظرات دانشجویان"/>
-          
+              <Comments helmetTitle="وب سایت شخصی | نظرات دانشجویان" />
             </Page>
             <Page pageNumber={pageNumber} index={4}>
-              <Contact helmetTitle={"وب سایت شخصی | ارتباط با من"}/>
+              <Contact helmetTitle={"وب سایت شخصی | ارتباط با من"} />
             </Page>
-       
           </PageContainer>
         </Layout>
       </div>
