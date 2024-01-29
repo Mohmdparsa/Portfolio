d1506e4317894127499a2d65790deb547b050382
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-01-29 12:52:27 -0800

    refactor(Myportfolio.jsx): remove portfolioInfo condition component

--- src/Pages/MyPortfolio.jsx
+++ src/Pages/MyPortfolio.jsx
@@ -46,54 +46,7 @@ const MyPortfolio = ({ helmetTitle, children }) => {
             text="نمونه کارها"
           />
           <Grid container sx={{ p: 3 }}>
-            {portfolioInfo.map((portfolio, index) => (
-              <Grid
-                key={index}
-                xs={12}
-                sm={6}
-                md={6}
-                lg={4}
-                sx={{ px: 2, mb: 2 }}
-              >
-                <Slide
-                  direction="up"
-                  in={loading}
-                  style={{
-                    transitionDelay: loading ? `${index + 3}99ms` : "0ms",
-                  }}
-                >
-                  <Card sx={{ maxWidth: 345, backgroundColor: "steelblue" }}>
-                    {/* new */}
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={portfolio.image}
-                        alt={portfolio.title}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {portfolio.title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          {portfolio.info}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                      <Button
-                        href={portfolio.link}
-                        size="small"
-                        color="primary"
-                        target="_blank"
-                      >
-                        اطلاعات بیشتر
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Slide>
-              </Grid>
-            ))}
+           
           </Grid>
         </CardContent>
       </Card>
