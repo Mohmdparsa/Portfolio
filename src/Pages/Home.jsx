5476ee55ae684298e94049d69a2d03be8c5bfe86
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-01-29 23:33:36 -0800

    refactor(Home.jsx): remove particle component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,6 @@ import PageImage1 from "../Assets/PageImage1.jpg";
 import { Typography, Box } from "@mui/material";
 import Typed from "typed.js";
 import { useRef, useEffect } from "react";
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import { Links } from "../Constans/Particles";
 import { Helmet } from "react-helmet-async";
 
 const Home = ({helmetTitle}) => {
@@ -37,13 +33,6 @@ const Home = ({helmetTitle}) => {
       typedName.destroy();
     };
   }, []);
-  const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
   return (
     <>
       <Box
@@ -58,12 +47,6 @@ const Home = ({helmetTitle}) => {
         <Helmet>
           <title>{helmetTitle}</title>
         </Helmet>
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={Links}
-        />
         <Typography
           ref={nameEl}
           variant="h3"
